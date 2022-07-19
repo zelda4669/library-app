5c3678ecbc2df3fdf2a531938b7330dd5713d900
Noelle Ferrari <ferrari.noelle@gmail.com>
2022-07-19 00:00:45 -0700

    Write form submission logic

--- library.js
+++ library.js
@@ -1,33 +1,27 @@
-const myLibrary = [
-    {'title': 'The Hobbit', 'author': 'J.R.R. Tolkein', 'read': 'read'},
-    {'title': 'A Game of Thrones', 'author': 'George R.R. Martin', 'read': 'read'},
-    {'title': 'Pride and Prejudice', 'author': 'Jane Austen', 'read': 'read'},
-]
+const myLibrary = []
 
 let shelf = document.querySelector('.shelf')
-// let newTitle = document.forms.title()
+let form = document.querySelector('form')
+form.addEventListener('submit', addBookToLibrary)
+
+let newTitle = document.querySelector('#title')
 let newAuthor = document.querySelector('#author')
 let isRead = document.querySelector('#read')
-let button = document.querySelector('#submit')
 
 class Book {
     constructor(title, author, read) {
         this.title = title
         this.author = author
         this.read = read
-
-        this.info = function () {
-            return `${title} by ${author}, ${read}.`
-        }
     }
 }
 
-button.addEventListener('click', addBookToLibrary)
-
 function addBookToLibrary() {
-    console.log(newTitle)
-    let book = new Book(newTitle, newAuthor, isRead)
+    let book = new Book(newTitle.value, newAuthor.value)
+    book.read = isRead.checked
     myLibrary.push(book)
+    generateBook(book)
+    form.reset()
 }
 
 
@@ -37,24 +31,41 @@ function generateBook(book) {
     let author = document.createElement('p')
     let readLabel = document.createElement('label')
     let checkbox = document.createElement('input')
+    let deleteBook = document.createElement('button')
+
+    deleteBook.classList.add('delete')
+    title.classList.add('title')
 
     checkbox.type = 'checkbox'
     checkbox.name = 'isRead'
     checkbox.id = 'isRead'
     readLabel.for = 'isRead'
 
+    checkbox.addEventListener('change', () => {
+        book.read = checkbox.checked
+        console.log(book)
+    })
+    
+    if(book.read) {
+        checkbox.checked = true
+    }
+
     item.classList.add('book')
     title.textContent = book.title
     author.textContent = book.author
     readLabel.textContent = 'read?'
+    deleteBook.textContent = 'X'
 
     item.appendChild(title)
     item.appendChild(author)
     item.appendChild(readLabel)
     item.appendChild(checkbox)
+    item.appendChild(deleteBook)
 
     shelf.appendChild(item)
 }
+
+
 
 for(let i=0; i<myLibrary.length; i++) {
     generateBook(myLibrary[i])
